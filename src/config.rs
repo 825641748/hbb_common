74abdb9b215fc30a373dde0d73aad6ff2fe1cb38
叻 <825641748@qq.com>
2026-07-30 16:57:51 +0800

    update Key

--- src/config.rs
+++ src/config.rs
@@ -120,7 +120,7 @@ const CHARS: &[char] = &[
 //pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 //pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.weizexin.top"];
-pub const RS_PUB_KEY: &str = "kMB9KQN+oInVNDSgl1Mns2JGEpVn3XKDsEE2D9Jz1Cw=";
+pub const RS_PUB_KEY: &str = "+Yh+wxuMQLZmWGhtK5C1dW0a+TdQknlyTZMYUam5lrM=";
 
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
